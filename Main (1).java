e440bba4b8c0de58de568dd3e3738f11018e736b
Lielgray <144587407+Lielgray@users.noreply.github.com>
2023-11-28 19:45:43 +0200

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,59 @@
+class Main {                    //תרגיל ראשון לכתוב 3 מספרים מטיפוס אינט להדפיס כול ערך בשורה נפרדת
+  public static void main(String[] args) { 
+    int tf = 5;
+    int z = 3;
+    int bg = 8;
+      
+    System.out.println(tf);
+    System.out.println(z);
+    System.out.println(bg);
+
+    //תרגיל שני, כתוב תוכנית המגדירה משתנה מפלואוט ומשתנה מסוג אינט התוכנית תדפיס עם רווח של שלוש תווים
+    int R  = 10;
+    float F = 2.50F;
+    System.out.println(R+"   "+F);
+
+    //כתוב תוכנית המגדירה מספר משני צאר כפי מספר האותיות של שמך התוכנית תדפיס באופן שיודפס שמך על המסך
+    char O = 'L';
+    char P = 'i';
+    char M = 'e';
+    System.out.println(O+""+P+""+M+""+O);
+
+    //תרגיל רביעי, כתוב תוכנית המגדירה 2 משתנים מספר שלם, תדפיס שהמספר הראשון קטן מהשני
+    int w = 20;
+    int g = 8;
+    System.out.println(w+"<"+g);
+      //תרגיל חמישי, כתוב שלוש משתנים מטיפוס אינט ותדפיס את הממוצע שלהם
+    int U = 6;
+    int OE = 35;
+    int OM = 83;
+    System.out.println((U+OE+OM)/3);
+
+    //כתוב תוכנית המגדירה אורך צלעות a ו - b של משולש ישר זווית(מטיפוס שלמים), התוכנית תדפיס
+    //את אורך צלע  c יתר בריבוע 
+
+    int a = 4*4;
+    int b = 9*9;
+    int c = a + b;
+    System.out.println(c);
+
+    // קיימת המשוואה הבאה: +5x2=y כתוב תוכנית המגדירה את x כרצונך(טיפוס שלם) ומדפיסה את
+    //y על פי הפונקציה.
+
+    int x = 5;
+    int y = 2*x+5;
+    System.out.println(y);
+
+    // תרגיל שמונה מופיע במצגת
+    First = "I like to study computer science";
+    Sec = "and therefore ready to invest";
+    Three = "hours a day for practice";
+    int hours = 5;
+    System.out.println(First, Sec, Hours, Three);
+
+     
+
+
+    
+  }
+}
